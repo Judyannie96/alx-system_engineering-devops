cd44247ea31704bf8f8ed82225e325b81945404d
Judyannie96 <judithochuba@gmai.com>
2024-07-03 22:08:03 -0700

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,17 +1,17 @@
 #install flask with puppet
 
-package {'python 3.8':
+package { 'python 3.8':
   ensure   => '3.8.10'
   provider => 'pip3',
 }
 
-package {'flask':
+package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
 
-package {'Werkzeug':
+package { 'werkzeug':
   ensure   => '2.1.1',
   provider => 'pip3',
-  require  => package['flask'],
+  require  => package['Flask'],
 }
